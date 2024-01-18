6eb742a0ac79480a3d61266755b807c64cf4c031
Dimitar9 <dimitar9909@gmail.com>
2024-01-18 00:44:12 -0800

    Update 1768. Merge Strings Alternately.java

--- 1768. Merge Strings Alternately.java
+++ 1768. Merge Strings Alternately.java
@@ -16,3 +16,35 @@ class Solution {
 
 }
 
+/*
+Rust
+
+impl Solution {
+    pub fn merge_alternately(word1: String, word2: String) -> String {
+        let mut chars1 = word1.chars();
+        let mut chars2 = word2.chars();
+        let mut ans = String::with_capacity(word1.len() + word2.len());
+        for _ in 0..word1.len().min(word2.len()) {
+            ans.push(chars1.next().unwrap());
+            ans.push(chars2.next().unwrap());
+        }
+        ans.extend(chars1);
+        ans.extend(chars2);
+        ans
+    }
+}
+
+Racket
+
+(define/contract (merge-alternately word1 word2)
+  (-> string? string? string?)
+    (list->string (merge-list-alternately (string->list word1) (string->list word2)))
+  )
+
+(define (merge-list-alternately wl1 wl2)
+    (cond ((empty? wl1) wl2)
+          ((empty? wl2) wl1)
+          (else (cons (car wl1) (cons (car wl2) (merge-list-alternately (cdr wl1) (cdr wl2))))))
+)
+
+*/
